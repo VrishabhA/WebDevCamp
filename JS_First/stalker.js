9ef23e8ae49412248d5b089d4ae518cea2d1ab8b
VrishabhA <vrishabh.alva@gmail.com>
2018-01-16 21:21:09 +0530

    Reconnecting with JS basics

---
+++ JS_First/stalker.js
@@ -0,0 +1,8 @@
+//Inputs 
+var fName = prompt("First name please.");
+var lName = prompt("Last name please");
+var age= prompt("What's you'r age");
+
+//Output
+console.log("Hello "+fName+" "+lName);
+console.log("Cool! so you're "+age+" years old");
